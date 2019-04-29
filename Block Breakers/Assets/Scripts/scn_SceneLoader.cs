aa1ab68af3164b37fa37c4b52972ef213fba9e7e
MarcoAntionioValerio <marco.valerio@leedscitycollege.ac.uk>
2019-04-29 11:58:24 +0100

    Scenes flow one to another correctly.

--- Block Breakers/Assets/Scripts/scn_SceneLoader.cs
+++ Block Breakers/Assets/Scripts/scn_SceneLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour {
+public class scn_SceneLoader : MonoBehaviour {
 
 	public void LoadNextScene()
     {
