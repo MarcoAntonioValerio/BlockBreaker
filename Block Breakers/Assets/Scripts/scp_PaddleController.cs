492ebdc15586d2d0813512aa3f0ea7bf6a557850
MarcoAntionioValerio <marco.valerio@leedscitycollege.ac.uk>
2019-05-01 13:18:08 +0100

    Refactored the PaddleController a bit.

--- Block Breakers/Assets/Scripts/scp_PaddleController.cs
+++ Block Breakers/Assets/Scripts/scp_PaddleController.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class scp_PaddleController : MonoBehaviour
 {
+    //Configuration Parameters
     [SerializeField] float screenWidthInUnits = 16f;
-    [SerializeField] float leftBoudary = 0f;
-    [SerializeField] float rightBoudary = 16f;
+    [SerializeField] float leftBoudary = 1f;
+    [SerializeField] float rightBoudary = 15f;
    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
+    {
+        MouseController();
+    }
+
+    void MouseController()
     {
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
