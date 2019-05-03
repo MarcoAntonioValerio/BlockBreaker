8742e990ee2eaf709eaa24a8094c9746efbc905f
MarcoAntionioValerio <marco.valerio@leedscitycollege.ac.uk>
2019-05-03 09:00:41 +0100

    Ball script created, ball is sticking to the paddle, with a bit of swaying.

---
+++ Block Breakers/Assets/Scripts/scp_Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scp_Ball : MonoBehaviour
+{
+    // Config Parameters
+    [SerializeField] scp_PaddleController paddle_1;
+
+    // States
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle_1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle_1.transform.position.x, paddle_1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
--- Block Breakers/Assets/Scripts/scp_PaddleController.cs
+++ Block Breakers/Assets/Scripts/scp_PaddleController.cs
@@ -11,7 +11,7 @@ public class scp_PaddleController : MonoBehaviour
    
 
     
-
+                                                                   
     // Update is called once per frame
     void Update()
     {
